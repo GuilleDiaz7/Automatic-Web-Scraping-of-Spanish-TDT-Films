1315e009fbaba8438f296a5031cd4ff42961a70c
Guillermo Díaz <42537388+GuilleDiaz7@users.noreply.github.com>
2022-09-13 12:31:53 +0200

    Update TDT-film_scraper.R
    Copy and paste the .R file from local and making some changes (bad way, I know).

--- TDT-film_scraper.R
+++ TDT-film_scraper.R
@@ -1,1 +1,67 @@
+#### LOAD PACKAGES ####
+library(rvest)
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(lubridate)
+
+
+#### LOAD URL AND SCRAPE THE WEBSITE ####
+url <- "https://www.elmundo.es/television/programacion-tv/peliculas.html"
+html_data <- read_html(url)
+
+films <- html_data %>% 
+  html_nodes(".nombre-programa a") %>% 
+  html_text2()
+
+times <- html_data %>% 
+  html_nodes(".hora-categoria") %>% 
+  html_text2()
+
+genre <- html_data %>% 
+  html_nodes(".titulo-categoria") %>% 
+  html_text2()
+
+description <- html_data %>% 
+  html_nodes(".sinopsis-programa") %>% 
+  html_text2()
+
+dates <- html_data %>% 
+  html_nodes("li.programa-canal") %>% 
+  html_attr("name")
+
+## This is not the best way to scrape this content, but I haven't figure out a better way yet ##
+channel <- html_data %>% 
+  html_nodes(xpath = '//meta[@itemprop="name"]') %>% 
+  html_attr("content")
+channel <- as.data.frame(channel)
+channel <- channel[seq_len(nrow(channel)) %% 2 == 0, ] 
+
+
+#### CREATE THE FINAL DATAFRAME AND CLEAN IT ####
+df <- as.data.frame(cbind(dates, times, channel, films, genre, description))
+
+df_clean <- df %>% 
+  filter(films != "Cine" & genre != "Cine")
+
+df_clean$dates <- substr(df_clean$dates, 1,8)
+
+df_clean$dates <- df_clean$dates %>% 
+  ymd()
+
+df_clean$date_time = ymd_hm(paste(df_clean$dates, df_clean$times))
+df_clean <- df_clean %>% select(
+  -c(times, dates)
+)
+df_clean <- df_clean %>% 
+  relocate(date_time, .before = channel)
+
+#### APPEND DATA DAY TO DAY TO A .CSV FILE ####
+write.table(prueba, "pelis_tv_hoy.csv", fileEncoding = "UTF-8", sep = ",", row.names = FALSE, append = TRUE, col.names = FALSE)
+
+## Check if the appending works fine ##
+prueba <- read.csv("pelis_tv_hoy.csv", fileEncoding = "UTF-8")
+  
+
 
