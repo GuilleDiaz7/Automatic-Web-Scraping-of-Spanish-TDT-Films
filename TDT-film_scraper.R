98ae2a58903acf367b985a1a7b142db45f0acfae
Guillermo Díaz <42537388+GuilleDiaz7@users.noreply.github.com>
2022-09-13 13:03:33 +0200

    Update TDT-film_scraper.R

--- TDT-film_scraper.R
+++ TDT-film_scraper.R
@@ -54,7 +54,7 @@ df_clean <- df %>%
             )
 
 #### APPEND DATA DAY TO DAY TO A .CSV FILE ####
-write.table(df_clean, "pelis_tv_hoy.csv", fileEncoding = "UTF-8", sep = ",", row.names = FALSE, append = TRUE, col.names = FALSE)
+write.table(df_clean, "pelis_tv_hoy.csv", fileEncoding = "UTF-8", sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
 
 ## Check if the appending works fine ##
 prueba <- read.csv("pelis_tv_hoy.csv", fileEncoding = "UTF-8")
