d5d3359a9d775bf076f0adaa992910c87cc49fe1
Guillermo Díaz <guillermo.dgr.7@gmail.com>
2022-09-18 14:36:23 +0200

    Update eda.R

--- eda.R
+++ eda.R
@@ -1,1 +1,9 @@
 #### EXPLORATORY DATA ANALYSIS OF THE SCRAPED DATA ####
+
+## Some questions and... some answers later ##
+
+#  QUESTIONS #
+
+# 1.- How many channels are there in the dataset? And film genres?
+# 2.- How many different films have been shown? Is there any repeated film?
+# 3.- How many times has each film been broadcasted?
