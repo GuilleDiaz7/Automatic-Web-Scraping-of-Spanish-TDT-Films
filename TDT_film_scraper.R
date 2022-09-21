ca088d35ce03f0b526ba2b29d6acc1ea397953b0
Guillermo Díaz <guillermo.dgr.7@gmail.com>
2022-09-21 16:53:03 +0200

    Update TDT_film_scraper.R

--- TDT_film_scraper.R
+++ TDT_film_scraper.R
@@ -8,44 +8,30 @@ library(lubridate)
 
 
 #### LOAD URL AND SCRAPE THE WEBSITE ####
-main_url <- "https://www.elmundo.es/television/programacion-tv/peliculas.html"
-html_data <- read_html(main_url)
+html_data <- read_html("https://www.elmundo.es/television/programacion-tv/peliculas.html")
 
-sp_title <- html_data %>% 
-  html_nodes(".nombre-programa a") %>% 
-  html_text2()
-
-time <- html_data %>% 
-  html_nodes(".hora-categoria") %>% 
-  html_text2()
-
-genre <- html_data %>% 
-  html_nodes(".titulo-categoria") %>% 
-  html_text2()
-
-description <- html_data %>% 
-  html_nodes(".sinopsis-programa") %>% 
-  html_text2()
-
-date <- html_data %>% 
-  html_nodes("li.programa-canal") %>% 
-  html_attr("name")
-
-## This is not the best way to scrape this content, but I haven't figure out a better way yet ##
-channel <- html_data %>% 
-  html_nodes(xpath = '//meta[@itemprop="name"]') %>% 
-  html_attr("content")
-channel <- as.data.frame(channel)
-channel <- channel[seq_len(nrow(channel)) %% 2 == 0, ] 
-
-url <- html_data %>% 
-  html_nodes(xpath = '//a[@itemprop="url"]') %>%
-  html_attr("href")
-url <- url[1:length(sp_title)]
-
-
-#### CREATE THE FINAL DATAFRAME AND CLEAN IT ####
-df <- as.data.frame(cbind(date, time, channel, sp_title, genre, url, description))
+df <- tibble(date = html_data %>% 
+                   html_nodes("li.programa-canal") %>% 
+                   html_attr("name"),
+                 time = html_data %>% 
+                   html_nodes(".hora-categoria") %>% 
+                   html_text2(),
+                 channel = html_data %>% 
+                   html_nodes(".programa-celda-1 div meta") %>% 
+                   html_attr("content"),
+                 sp_title = html_data %>% 
+                   html_nodes(".nombre-programa a") %>% 
+                   html_text2(), 
+                 genre = html_data %>% 
+                   html_nodes(".titulo-categoria") %>% 
+                   html_text2(),
+                 description = html_data %>% 
+                   html_nodes(".sinopsis-programa") %>% 
+                   html_text2(),
+                 url = html_data %>% 
+                   html_nodes(".nombre-programa a") %>%
+                   html_attr("href")
+                 )
 
 df_clean <- df %>% 
   filter(sp_title != "Cine" & genre != "Cine") %>% 
@@ -56,8 +42,8 @@ df_clean <- df %>%
             channel = channel,
             sp_title = sp_title, 
             genre = genre,
-            url = url,
-            description = description
+            description = description,
+            url = url
   )
 
 
@@ -73,98 +59,86 @@ nav_results_list <- tibble(
 )
 
 results_by_film_url <- tibble(url = nav_results_list$url,
-                              original_title = map(nav_results_list$html_result,
-                                                   ~ .x %>%
-                                                     html_nodes("tr:nth-child(2) .ficha-txt-descripcion") %>%
-                                                     html_text2()
-                              ),
-                              year = map(nav_results_list$html_result,
-                                         ~ .x %>%
-                                           html_nodes("tr:nth-child(4) .ficha-txt-descripcion") %>%
-                                           html_text2()
-                              ),
-                              country = map(nav_results_list$html_result,
-                                            ~ .x %>%
-                                              html_nodes("tr:nth-child(3) .ficha-txt-descripcion") %>%
-                                              html_text2()
-                              ),
-                              length = map(nav_results_list$html_result,
-                                           ~ .x %>%
-                                             html_nodes("tr:nth-child(5) .ficha-txt-descripcion") %>%
-                                             html_text2()
-                              )    
-)
+                             casillas = map(nav_results_list$html_result,
+                                                             ~ .x %>%
+                                                               html_nodes(".ficha-txt") %>%
+                                                               html_text2()
+                             ),
+                             datos = map(nav_results_list$html_result,
+                                                         ~ .x %>%
+                                                           html_nodes(".ficha-txt-descripcion") %>%
+                                                           html_text2()
+                             )
+                             )
 
-joined_tibble <- left_join(
-  df_clean, results_by_film_url, by = c("url" = "url")
-)
+results_by_film_url <- results_by_film_url %>%
+  unnest(c(casillas, datos)) %>% 
+  mutate(datos = ifelse(datos == "", NA, datos)) %>% 
+  pivot_wider(names_from = casillas, values_from = datos) %>% 
+  select(c(1, 3:13))
 
-
-joined_tibble <- joined_tibble %>% 
-  mutate(country = unlist(country),
-         length = unlist(length))
-
-joined_tibble <- joined_tibble %>%
-  mutate(across(3:9, ~ifelse(.=="", NA, as.character(.))))
-joined_tibble$year <- as.integer(joined_tibble$year)
-joined_tibble <- joined_tibble %>% 
-  relocate(c("country", "length", "year"), .before = description)
-
-df_final <- joined_tibble %>% 
+df_final <- left_join(df_clean, results_by_film_url, bu = c("url", "url")) %>% 
+  select(-url) %>% 
+  mutate(`Año:` = as.integer(`Año:`),
+         `Duración:` = as.integer(parse_number(`Duración:`))) %>% 
+  dplyr:: rename(
+           actors = "Intérprete:",
+           country = "País:",
+           director = "Director:",
+           length = "Duración:",
+           music = "Música:",
+           original_title = "Título original:",
+           photography = "Director de fotografía:", 
+           producer = "Producción:",
+           production_company = "Productora:",
+           writer = "Guión:",
+           year = "Año:"
+         ) %>% 
+  relocate(
+    original_title, .after = sp_title
+  ) %>% 
   relocate(
     year, .before = genre
   ) %>% 
   relocate(
-    original_title, .after = sp_title
+    c("country", "length"), .before = description
   ) %>% 
-  select(-url) %>% 
-  mutate(original_title = as.character(original_title),
-         year = as.numeric(year))
+  relocate(
+    director, .before = writer
+  ) %>% 
+  relocate(
+    c("photography", "music", "producer", "production_company"), .before = actors
+  )
 
 #### APPEND DATA DAY TO DAY TO A .CSV FILE ####
 write.table(df_final, "data/pelis_tv_hoy.csv", fileEncoding = "UTF-8", sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
 
 #### LOAD URL AND SCRAPE THE WEBSITE ####
-main_url <- "https://www.elmundo.es/television/programacion-tv/be-mad.html"
-html_data <- read_html(main_url)
+html_data <- read_html("https://www.elmundo.es/television/programacion-tv/be-mad.html")
 
-sp_title <- html_data %>% 
-  html_nodes(".nombre-programa a") %>% 
-  html_text2()
-
-time <- html_data %>% 
-  html_nodes(".hora-categoria") %>% 
-  html_text2()
-
-genre <- html_data %>% 
-  html_nodes(".titulo-categoria") %>% 
-  html_text2()
-
-description <- html_data %>% 
-  html_nodes(".sinopsis-programa") %>% 
-  html_text2()
-
-date <- html_data %>% 
-  html_nodes("li.programa-canal") %>% 
-  html_attr("name")
-
-## This is not the best way to scrape this content, but I haven't figure out a better way yet ##
-channel <- html_data %>% 
-  html_nodes(xpath = '//meta[@itemprop="name"]') %>% 
-  html_attr("content")
-channel <- as.data.frame(channel)
-channel <- channel[seq_len(nrow(channel)) %% 2 == 0, ] 
-
-url <- html_data %>% 
-  html_nodes(xpath = '//a[@itemprop="url"]') %>%
-  html_attr("href")
-url <- url[1:length(sp_title)]
-
-#### CREATE THE FINAL DATAFRAME AND CLEAN IT ####
-df <- as.data.frame(cbind(date, time, channel, sp_title, genre, url, description))
+df <- tibble(date = html_data %>% 
+               html_nodes("li.programa-canal") %>% 
+               html_attr("name"),
+             time = html_data %>% 
+               html_nodes(".hora-categoria") %>% 
+               html_text2(),
+             channel = "Be Mad",
+             sp_title = html_data %>% 
+               html_nodes(".nombre-programa a") %>% 
+               html_text2(), 
+             genre = html_data %>% 
+               html_nodes(".titulo-categoria") %>% 
+               html_text2(),
+             description = html_data %>% 
+               html_nodes(".sinopsis-programa") %>% 
+               html_text2(),
+             url = html_data %>% 
+               html_nodes(".nombre-programa a") %>%
+               html_attr("href")
+)
 
 df_clean <- df %>% 
-  filter(sp_title != "Cine" & !genre %in%  c("Cine", "Otros", "Variedades")) %>% 
+  filter(sp_title != "Cine" & genre != "Cine") %>% 
   mutate(date = ymd(substr(date, 1, 8)),
   ) %>%
   filter(date == Sys.Date() ) %>%
@@ -172,9 +146,10 @@ df_clean <- df %>%
             channel = channel,
             sp_title = sp_title, 
             genre = genre,
-            url = url,
-            description = description
+            description = description,
+            url = url
   )
+
 
 #### Perform the second part of the scraping ####
 nav_results_list <- tibble(
@@ -188,53 +163,55 @@ nav_results_list <- tibble(
 )
 
 results_by_film_url <- tibble(url = nav_results_list$url,
-                              original_title = map(nav_results_list$html_result,
-                                                   ~ .x %>%
-                                                     html_nodes("tr:nth-child(2) .ficha-txt-descripcion") %>%
-                                                     html_text2()
+                              casillas = map(nav_results_list$html_result,
+                                             ~ .x %>%
+                                               html_nodes(".ficha-txt") %>%
+                                               html_text2()
                               ),
-                              year = map(nav_results_list$html_result,
-                                         ~ .x %>%
-                                           html_nodes("tr:nth-child(4) .ficha-txt-descripcion") %>%
-                                           html_text2()
-                              ),
-                              country = map(nav_results_list$html_result,
-                                            ~ .x %>%
-                                              html_nodes("tr:nth-child(3) .ficha-txt-descripcion") %>%
-                                              html_text2()
-                              ),
-                              length = map(nav_results_list$html_result,
-                                           ~ .x %>%
-                                             html_nodes("tr:nth-child(5) .ficha-txt-descripcion") %>%
-                                             html_text2()
-                              )    
+                              datos = map(nav_results_list$html_result,
+                                          ~ .x %>%
+                                            html_nodes(".ficha-txt-descripcion") %>%
+                                            html_text2()
+                              )
 )
 
-joined_tibble <- left_join(
-  df_clean, results_by_film_url, by = c("url" = "url")
-)
+results_by_film_url <- results_by_film_url %>%
+  unnest(c(casillas, datos)) %>% 
+  mutate(datos = ifelse(datos == "", NA, datos)) %>% 
+  pivot_wider(names_from = casillas, values_from = datos) %>% 
+  select(c(1, 3:13))
 
-
-joined_tibble <- joined_tibble %>% 
-  mutate(country = unlist(country),
-         length = unlist(length))
-
-joined_tibble <- joined_tibble %>%
-  mutate(across(3:9, ~ifelse(.=="", NA, as.character(.))))
-joined_tibble$year <- as.integer(joined_tibble$year)
-joined_tibble <- joined_tibble %>% 
-  relocate(c("country", "length", "year"), .before = description)
-
-df_final <- joined_tibble %>% 
-  relocate(
-    year, .before = genre
+df_final <- left_join(df_clean, results_by_film_url, bu = c("url", "url")) %>% 
+  select(-url) %>% 
+  mutate(`Año:` = as.integer(`Año:`),
+         `Duración:` = as.integer(parse_number(`Duración:`))) %>% 
+  dplyr:: rename(
+    actors = "Intérprete:",
+    country = "País:",
+    director = "Director:",
+    length = "Duración:",
+    music = "Música:",
+    original_title = "Título original:",
+    photography = "Director de fotografía:", 
+    producer = "Producción:",
+    production_company = "Productora:",
+    writer = "Guión:",
+    year = "Año:"
   ) %>% 
   relocate(
     original_title, .after = sp_title
   ) %>% 
-  select(-url) %>% 
-  mutate(original_title = as.character(original_title),
-         year = as.numeric(year))
+  relocate(
+    year, .before = genre
+  ) %>% 
+  relocate(
+    c("country", "length"), .before = description
+  ) %>% 
+  relocate(
+    director, .before = writer
+  ) %>% 
+  relocate(
+    c("photography", "music", "producer", "production_company"), .before = actors
+  )
 
-#### APPEND DATA DAY TO DAY TO A .CSV FILE ####
 write.table(df_final, "data/pelis_tv_hoy.csv", fileEncoding = "UTF-8", sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
